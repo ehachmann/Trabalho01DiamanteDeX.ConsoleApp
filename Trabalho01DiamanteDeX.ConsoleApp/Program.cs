a19c521c1a4142f611cd431bfe8a475881c66a1c
ehachmann <ehachmann@hotmail.com>
2025-04-10 20:49:22 -0300

    Rotina de execução que faz o desenho do diamante de X

--- Trabalho01DiamanteDeX.ConsoleApp/Program.cs
+++ Trabalho01DiamanteDeX.ConsoleApp/Program.cs
@@ -4,16 +4,18 @@ namespace Trabalho01DiamanteDeX.ConsoleApp
 {
     internal class Program
     {
-        // Loop de validação se o número é par ou menor que 1
+        // Rotina de execução que faz o desenho do diamante de X
         static void Main(string[] args)
         {
             // Variável para controle de repetição
             string numeroinvalido = "S";
 
             while (numeroinvalido == "S")
-            { 
+            {
+                Console.Clear();
 
                 // Entrada de dados
+
                 Console.WriteLine("Método do Diamante de X");
                 Console.WriteLine("OBS: O número deve ser ímpar e maior que 1");
                 Console.Write("Digite o número: ");
@@ -23,18 +25,58 @@ namespace Trabalho01DiamanteDeX.ConsoleApp
                 int espaco = numero / 2;
             
                 //Verifica se o número é par e maior que 1
+
                 if (numero <= 1)
                 {
                     Console.WriteLine(); 
                     Console.WriteLine("Número inválido, digite um número maior que 1 !!!");
+                    Console.ReadKey();
+                    continue;
                 }
                 else if (numero % 2 == 0)
                 {
                     Console.WriteLine();
                     Console.WriteLine("Número inválido, digite um número ímpar !!!");
+                    Console.ReadKey();
+                    continue;
                 }
 
+                Console.WriteLine();
 
+                // Desenha a parte superior do diamante
+
+                for (int i = 0; i <= meio; i++)
+                {
+                    for (int j = espaco; j >= 1; j--)
+                    {
+                        Console.Write(" ");
+                    }
+                    espaco--;
+                    for (int j = 1; j <= (2 * i + 1); j++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+
+                // Desenha a parte inferior do diamante
+
+                espaco = 1;
+                for (int i = meio - 1; i >= 0; i--)
+                {
+                    for (int j = 1; j <= espaco; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                    espaco++;
+                    for (int j = 1; j <= (2 * i + 1); j++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+
+             Console.WriteLine();
              Console.Write("Digitar novamente? (S/N): ");
              string opcaoContinuar = Console.ReadLine().ToUpper();
 
